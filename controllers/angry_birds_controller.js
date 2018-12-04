9db606b2e9ddffe6abaa5174c2b61f361a0c6a0d
abdulrabbt <abdulrab.bt@hotmail.com>
2018-12-04 23:39:43 +0300

    hw w04d03

---
+++ controllers/angry_birds_controller.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var angryBirds = require('../modules/angry_birds');
+
+router.get('/', function(request, response){
+    var mustacheVariables = {
+        angryBirdsList: angryBirds.birds
+    }
+    response.render('./angry_birds/index');
+});
+
+router.get('/:id', function(){
+    var angryBirdsId = request.params.id;
+    var angrybirds = angryBirds.birds[angryBirdsId];
+    var angryBirdsVariables = {
+        id: angryBirdsId,
+        name: angrybirds.name,
+        abilities: angrybirds.abilities,
+        image: angrybirds.image,
+        group: angrybirds.group,
+        species: angrybirds.species,
+        feels: angrybirds.feels
+    }
+    response.render('./angry_birds/show', angryBirdsVariables);
+})
+
+
+module.exports = router;
